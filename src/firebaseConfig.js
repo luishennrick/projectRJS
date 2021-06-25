a153fc72c778250846f070cf22fba73b86d89155
Luís Henrique <lhfreya@gmail.com>
2021-06-25 20:54:54 -0300

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,10 +1,10 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default  {
-    apiKey: "AIzaSyBv41XDm9m2s6CPVbr_Hc-RT0AVRnbXozY",
+    apiKey: "",
     authDomain: "projectrjs-9fe5d.firebaseapp.com",
     projectId: "projectrjs-9fe5d",
     storageBucket: "projectrjs-9fe5d.appspot.com",
-    messagingSenderId: "1012319346218",
-    appId: "1:1012319346218:web:2f98b5350523f297e7ad53",
+    messagingSenderId: "10123",
+    appId: "1",
     measurementId: "G-1PXGFKZ0R7"
   };
